ecc3c9b9c7977168b60e90e1d31e8cd2bc6cb6e5
steadmatthew <65732060+steadmatthew@users.noreply.github.com>
2020-07-22 23:17:13 +0100

    Delete privacypolicy.js

--- commands/privacypolicy.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-exports.run = (client, message, args) => {
-    const embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setDescription("[Privacy Policy](https://legal.animalbot.xyz)")
-    message.channel.send(embed).catch(console.error);
-}
